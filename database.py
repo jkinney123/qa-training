7791d50cd1b88ce234046f8a4e6ed77c02d0365a
Joe Kinney <48601772+jkinney123@users.noreply.github.com>
2024-11-08 20:48:27 +0000

    added two more columns to existing table with an update_user function, removed users with delete_user function.

--- database.py
+++ database.py
@@ -14,6 +14,7 @@ def create_database():
     name TEXT,
     age INTEGER
                 )''')
+          #  cursor.execute("ALTER TABLE users ADD COLUMN city TEXT;") 
 
             # Commit the changes
             conn.commit()
@@ -24,14 +25,39 @@ def create_database():
             print("SQLite database created successfully!")
 
 
-def create_user():
+
+def create_user(name, age):
             conn = sqlite3.connect('mydatabase.db')
             c = conn.cursor()
             c.execute("INSERT INTO users (name, age) VALUES (?, ?)",
-                      ('Alice', 30))
+                      (name, age))
 
             conn.commit()
             conn.close()
+
+def update_user(user_id, city):
+    """Updates the city for a user in the database.
+    Args:
+        user_id (int): The ID of the user to update.
+        city (str): The new city for the user.
+    """
+    conn = sqlite3.connect('mydatabase.db')
+    c = conn.cursor()
+    c.execute("UPDATE users SET city = ? WHERE id = ?", (city, user_id))
+    conn.commit()
+    conn.close()
+
+
+def delete_user(name, city):
+    """Deletes a user from the database based on their name.
+    Args:
+        name (str): The name of the user to delete.
+    """
+    conn = sqlite3.connect('mydatabase.db')
+    c = conn.cursor()
+    c.execute("DELETE FROM users WHERE name = ? AND city = ?", (name, city))
+    conn.commit()
+    conn.close()
 
 
 def print_database():
@@ -40,7 +66,7 @@ def print_database():
             # Create a cursor object to execute SQL commands
             cursor = conn.cursor()
             # Execute a SELECT query to retrieve all rows from the table
-            cursor.execute("SELECT * FROM users WHERE ID = 2")
+            cursor.execute("SELECT * FROM users")
             # Fetch all rows from the result set
             rows = cursor.fetchall()
             # Print the rows
--- main.py
+++ main.py
@@ -2,5 +2,18 @@ import database
 
 # Create the database if it doesn't exist
 database.create_database()
-database.create_user()
+
+# database.create_user('Joe', 40)
+# database.create_user('Tom', 30) 
+# database.create_user('Bob', 20) 
+
+database.update_user(1, 'New York')  # Update user with ID 1
+database.update_user(2, 'Chicago')
+database.update_user(3, 'San francisco')
+database.update_user(4, 'Minneapolis')
+# database.update_user(5, 'Seattle')
+
+database.delete_user('Alice', 'San francisco')
+
+
 database.print_database()
