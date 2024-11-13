312b77396501a18c86097cd0b2ce72cb64cf50d8
Joe Kinney <48601772+jkinney123@users.noreply.github.com>
2024-11-13 21:10:09 +0000

    removed duplicate users, updated user location info

--- main.py
+++ main.py
@@ -7,13 +7,11 @@ database.create_database()
 # database.create_user('Tom', 30) 
 # database.create_user('Bob', 20) 
 
-database.update_user(1, 'New York')  # Update user with ID 1
-database.update_user(2, 'Chicago')
-database.update_user(3, 'San francisco')
-database.update_user(4, 'Minneapolis')
+database.update_user(7, 'Los Angeles') 
+database.update_user(6, 'Reseda')
 # database.update_user(5, 'Seattle')
 
-database.delete_user('Alice', 'San francisco')
+
 
 
 database.print_database()
