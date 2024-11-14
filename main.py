22637601a40b2fdca45507846c93391801bdc5f6
Joe Kinney <48601772+jkinney123@users.noreply.github.com>
2024-11-14 00:54:18 +0000

    removed DB functions, now testing python functions

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
-import database
+import random
 
-# Create the database if it doesn't exist
-database.create_database()
+guesses_made = 0
 
-# database.create_user('Joe', 40)
-# database.create_user('Tom', 30) 
-# database.create_user('Bob', 20) 
+name = input('Hello! What is your name?\n')
 
-database.update_user(7, 'Los Angeles') 
-database.update_user(6, 'Reseda')
-# database.update_user(5, 'Seattle')
+number = random.randint(1, 20)
+print ('Well, {0}, I am thinking of a number between 1 and 20.'.format(name))
 
+while guesses_made < 6:
 
+    guess = int(input('Take a guess: '))
 
+    guesses_made += 1
 
-database.print_database()
+    if guess < number:
+        print ('Your guess is too low.')
+
+    if guess > number:
+        print ('Your guess is too high.')
+
+    if guess == number:
+        break
+
+if guess == number:
+    print ('Good job, {0}! You guessed my number in {1} guesses!'.format(name, guesses_made))
+else:
+    print ('Nope. The number I was thinking of was {0}'.format(number))
